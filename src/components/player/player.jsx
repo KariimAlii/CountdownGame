16414dbc848dd3d6e287da1b4a06b0c10962e213
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-20 14:35:10 +0300

    Refs (2) : DOM Manipulation -> Clear the Input after click

--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -7,6 +7,7 @@ export default function Player() {
     function handleClick() {
         // refPlayerName.current  ---> Native Input Html Element
         setPlayerName(refPlayerName.current.value);
+        refPlayerName.current.value = '';
     }
 
     return (
