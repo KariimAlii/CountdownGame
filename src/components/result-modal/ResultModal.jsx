74035a3053575f5c36fe05df14bb5587757d5d25
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-20 17:55:18 +0300

    Handle User Win & Calculate Score

--- src/components/result-modal/ResultModal.jsx
+++ src/components/result-modal/ResultModal.jsx
@@ -8,6 +8,8 @@ const ResultModal =  forwardRef(
 
         const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
+        const score = Math.round((1 - (remainingTime / (targetTime * 1000))) * 100);
+
         useImperativeHandle(ref, () => {
             return {
                 open() {
@@ -18,6 +20,7 @@ const ResultModal =  forwardRef(
         return (
             <dialog ref={dialog} className="result-modal">
                 {userLost && <h2>You lost !</h2>}
+                {!userLost && <h2>Your Score : {score}</h2>}
                 <p>
                     The target time was
                     <strong>{targetTime} seconds.</strong>
