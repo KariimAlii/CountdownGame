93df9ec5267599c9e3d09ff48ea22179daff634d
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-20 13:57:51 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Player from "./components/Player/player.jsx";
+
+function App() {
+    return (
+        <>
+            <Player />
+            <div id="challenges"></div>
+        </>
+    );
+}
+
+export default App;
